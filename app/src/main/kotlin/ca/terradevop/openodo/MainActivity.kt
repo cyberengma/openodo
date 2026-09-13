cae6de8a93fa5d7306d4066e5b62d64571ba44ba
Enigma <cyberengma@pm.me>
2026-09-13 06:50:30 +0300

    Fix Hilt ViewModel acquisition in activity

--- app/src/main/kotlin/ca/terradevop/openodo/MainActivity.kt
+++ app/src/main/kotlin/ca/terradevop/openodo/MainActivity.kt
@@ -5,17 +5,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.activity.viewModels
 import ca.terradevop.openodo.ui.theme.OpenodoTheme
 import ca.terradevop.openodo.ui.OpenOdoApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val appViewModel: ca.terradevop.openodo.ui.AppViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OpenodoTheme(darkTheme = isSystemInDarkTheme()) { OpenOdoApp(viewModel()) }
+            OpenodoTheme(darkTheme = isSystemInDarkTheme()) { OpenOdoApp(appViewModel) }
         }
     }
 }
